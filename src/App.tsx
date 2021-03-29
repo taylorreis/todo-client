97208ffb7c4ab853b13f34d263ebf4ee16a5b7a9
Taylor Reis <josephtreis@gmail.com>
2021-03-28 20:58:32 -0600

    Add a login form skeleton

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,30 @@
-import React from "react";
+import React, { Dispatch, SetStateAction, useState } from "react";
 import { QueryClient, QueryClientProvider } from "react-query";
 
+import LoginForm from "./components/LoginForm";
 import Todos from "./components/Todos";
 
 const queryClient = new QueryClient();
 
+const getComponent = (
+  isLoggedIn: boolean,
+  setIsLoggedIn: Dispatch<SetStateAction<boolean>>
+) => {
+  if (isLoggedIn) {
+    return (
+      <QueryClientProvider client={queryClient}>
+        <Todos />
+      </QueryClientProvider>
+    );
+  }
+
+  return <LoginForm setIsLoggedIn={setIsLoggedIn} />;
+};
+
 function App() {
-  return (
-    <QueryClientProvider client={queryClient}>
-      <Todos />
-    </QueryClientProvider>
-  );
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  return getComponent(isLoggedIn, setIsLoggedIn);
 }
 
 export default App;
